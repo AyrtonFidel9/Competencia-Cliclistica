e528c64b7a7f9859aff3c20aa225503b3b9b1aee
Ayrton Avalos <ayrtonfidel9@gmail.com>
2020-12-02 09:37:44 -0500

    Se ha agregado la carpeta para abrir IngresoCompetencia

--- WpfAppCompetenciaCiclistica/WpfAppCompetenciaCiclistica/Controles/Competencia.xaml.cs
+++ WpfAppCompetenciaCiclistica/WpfAppCompetenciaCiclistica/Controles/Competencia.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfAppCompetenciaCiclistica.Controles;
 
 namespace WpfAppCompetenciaCiclistica.Controles
 {
